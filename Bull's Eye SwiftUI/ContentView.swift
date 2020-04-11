9b03efe4c8169603429fe2c6653265023db3ad94
mohammad <mohammadnajafzade@yahoo.com>
2020-04-12 00:05:21 +0430

    Initial Commit

---
+++ Bull's Eye SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bull's Eye SwiftUI
+//
+//  Created by Mohammad Najafzade on 4/12/20.
+//  Copyright © 2020 mohammadn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
