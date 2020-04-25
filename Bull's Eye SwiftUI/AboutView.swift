b3ee71a7ed62b233fbed228015016fd3c8444d05
mohammad <mohammadnajafzade@yahoo.com>
2020-04-25 17:57:03 +0430

    add app icons and style the about page

--- Bull's Eye SwiftUI/AboutView.swift
+++ Bull's Eye SwiftUI/AboutView.swift
@@ -9,16 +9,45 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        VStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    let lightOrange = Color(red: 255.0 / 255.0, green: 214.0 / 255.0, blue: 179.0 / 255.0)
+    
+    struct AboutHeadingStyle: ViewModifier {
+        func body(content: Content) -> some View{
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("ArialRoundedMTBold", size: 30).bold())
+                .padding(.vertical, 20)
         }
-    .navigationBarTitle("About view")
+    }
+    
+    struct AboutBodyStyle: ViewModifier {
+        func body(content: Content) -> some View{
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("ArialRoundedMTBold", size: 16).bold())
+                .padding(.horizontal, 60)
+                .padding(.bottom, 20)
+        }
+    }
+    
+    var body: some View {
+        Group{
+            VStack{
+                Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle())
+                Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.").modifier(AboutBodyStyle())
+                Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.").modifier(AboutBodyStyle())
+                Text("Enjoy!").modifier(AboutBodyStyle())
+            }
+            .background(lightOrange)
+            .navigationBarTitle("About view")
+        }
+        .background(Image("Background"))
     }
 }
 
 struct AboutView_Previews: PreviewProvider {
     static var previews: some View {
-        AboutView()
+        AboutView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
